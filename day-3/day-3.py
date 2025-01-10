1a3d3bc380b6a427ff45f3722648d33a1a2dd545
daniprieto <dani101val@gmail.com>
2025-01-10 19:19:37 +0100

    exercise 3 part 2

--- day-3/day-3.py
+++ day-3/day-3.py
@@ -7,10 +7,13 @@ def main():
         data = file.read()
 
     pattern = r'mul\(\d{1,3},\d{1,3}\)'
-    pattern2 = r"do\(\)|don't\(\)|mul\(\d{1,3},\d{1,3}\)"
-    result: int = find_occurences(data, pattern)
+    result1: int = find_occurences(data, pattern)
+    print(f'Part 1 result: {result1}')
 
-    print(result)
+    pattern2 = r"do\(\)|don't\(\)|mul\(\d{1,3},\d{1,3}\)"
+    result2: int = find_better_ocurrences(data, pattern2)
+    print(f'Part 2 result: {result2}')
+    
 
 
 def find_occurences(data, pattern):
@@ -24,7 +27,26 @@ def find_occurences(data, pattern):
     return sol
 
 def find_better_ocurrences(data, pattern):
-    pass
+    matches: list[str] = re.findall(pattern=pattern, string=data)
+
+    sol: int = 0
+
+    allowed: bool = True
+
+    for match in matches:
+        match = match.strip()
+        if match == 'do()':
+            allowed = True
+        elif match == "don't()":
+            print('dont')
+            allowed = False
+        elif match.startswith('mul') and allowed:
+            numbers = re.findall(r'\d{1,3}', match)
+            num1, num2 = int(numbers[0]), int(numbers[1])
+            sol += num1 * num2
+    
+    return sol
+
 
 if __name__ == '__main__':
     main()
